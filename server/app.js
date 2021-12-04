734655d52a142b7436a3a358e3125e64936ca1e3
Elliop <abdelmouhayminegx@gmail.com>
2021-12-04 19:21:23 +0100

    Add Root Query

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
@@ -7,7 +8,7 @@ const app = express();
 app.use(
   "/graphql",
   graphqlHTTP({
-    // pass in a schema property
+    schema,
   })
 );
 
