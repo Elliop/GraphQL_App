2a41a5cf8ef9f95db44d937fa2e47d062a01ad40
Elliop <abdelmouhayminegx@gmail.com>
2021-12-05 16:27:33 +0100

    Apollo Client Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
+import { ApolloClient, ApolloProvider } from "@apollo/client";
 import BookList from "./components/BookList";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
 
 const App = () => {
   return (
-    <div>
-      {/*  */}
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 };
 
