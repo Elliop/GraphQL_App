3a0a0a55b50d5502f4d31a399c4c034df55bffb0
Elliop <abdelmouhayminegx@gmail.com>
2021-12-05 14:20:21 +0100

    Co to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connected!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 // bind express with graphql
 app.use(
