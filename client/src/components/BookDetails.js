3be4b0ad9a2cb80170a980ebdb84f1de6b9a0653
Elliop <abdelmouhayminegx@gmail.com>
2021-12-06 00:38:39 +0100

    Show Book Details (Making a Single Query)

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -1,11 +1,34 @@
+import { useQuery } from "@apollo/client";
 import { getBookQuery } from "../queries/queries";
 
-const BookDetails = () => {
-  return (
-    <div id="book-details">
-      <p>Output book details here</p>
-    </div>
-  );
+const BookDetails = (props) => {
+  const { data, loading } = useQuery(getBookQuery, {
+    variables: { id: props.bookId },
+  });
+
+  const displayBookDetails = () => {
+    if (!loading) {
+      if (data.book) {
+        return (
+          <div>
+            <h2>{data.book.name}</h2>
+            <p>{data.book.genre}</p>
+            <p>{data.book.author.name}</p>
+            <p>All books by this author:</p>
+            <ul className="other-books">
+              {data.book.author.books.map((item) => {
+                return <li key={item.id}>{item.name}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      } else {
+        return <div>No book selected...</div>;
+      }
+    }
+  };
+
+  return <div id="book-details">{displayBookDetails()}</div>;
 };
 
 export default BookDetails;
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,22 +1,33 @@
 import { useQuery } from "@apollo/client";
+import { useState } from "react";
 import { getBooksQuery } from "../queries/queries";
 import BookDetails from "./BookDetails";
 
 const BookList = () => {
   const { loading, data } = useQuery(getBooksQuery);
+  const [selected, setSelected] = useState();
   const displayBooks = () => {
     if (loading) {
       return <div>Loading books...</div>;
     } else {
       return data.books.map((book) => {
-        return <li key={book.id}>{book.name}</li>;
+        return (
+          <li
+            onClick={() => {
+              setSelected(book.id);
+            }}
+            key={book.id}
+          >
+            {book.name}
+          </li>
+        );
       });
     }
   };
   return (
     <div>
       <ul id="book-list">{displayBooks()}</ul>
-      <BookDetails />
+      <BookDetails bookId={selected} />
     </div>
   );
 };
